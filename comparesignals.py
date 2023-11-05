c9840a6d0b525ac5c910f3b4fc10c0cf39ede991
Omar Elgebali <99504626+OmarElgebali@users.noreply.github.com>
2023-11-05 02:04:57 +0200

    Revert "Revert "Task 4""

--- comparesignals.py
+++ comparesignals.py
@@ -1,7 +1,6 @@
-
-def SignalSamplesAreEqual(file_name,indices,samples):
-    expected_indices=[]
-    expected_samples=[]
+def SignalSamplesAreEqual(file_name, indices, samples):
+    expected_indices = []
+    expected_samples = []
     with open(file_name, 'r') as f:
         line = f.readline()
         line = f.readline()
@@ -9,11 +8,11 @@ def SignalSamplesAreEqual(file_name,indices,samples):
         line = f.readline()
         while line:
             # process line
-            L=line.strip()
-            if len(L.split(' '))==2:
-                L=line.split(' ')
-                V1=int(L[0])
-                V2=float(L[1])
+            L = line.strip()
+            if len(L.split(' ')) == 2:
+                L = line.split(' ')
+                V1 = int(L[0])
+                V2 = float(L[1])
                 expected_indices.append(V1)
                 expected_samples.append(V2)
                 line = f.readline()
@@ -27,6 +26,6 @@ def SignalSamplesAreEqual(file_name,indices,samples):
         if abs(samples[i] - expected_samples[i]) < 0.01:
             continue
         else:
-            print("Test case failed, your signal have different values from the expected one") 
+            print("Test case failed, your signal have different values from the expected one")
             return
     print("Test case passed successfully")
